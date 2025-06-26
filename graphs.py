2d35e0222b39d8b7bc4709db5ab6b77bb05a7605
Wiktor Trybus <wiktortrybus11@gmail.com>
2025-06-26 22:47:56 +0200

    Grafy cz.3

--- graphs.py
+++ graphs.py
@@ -32,9 +32,9 @@
 # 2. Sortowanie topologiczne
 # 3. Wyznaczanie silnie spójnych składowych (strongly connected components)
 # 4. Minimalne drzewo rozpinające (minimum spanning tree), algorytm Kruskala, Prima
-# 5. Las zbiorów rozłącznych (nie algorytm, a struktura danych dla zbiorów rozłącznych)
-# 6. Najkrótsze ścieżki z jednym źródłem, algorytm Bellmana-Forda, Dijkstry
-# 7. Najkrótsze ścieżki między wszystkimi parami wierzchołków, algorytm Floyda-Warshalla
+# 4.1. Las zbiorów rozłącznych (nie algorytm, a struktura danych dla zbiorów rozłącznych)
+# 5. Najkrótsze ścieżki z jednym źródłem, algorytm Bellmana-Forda, Dijkstry
+# 6. Najkrótsze ścieżki między wszystkimi parami wierzchołków, algorytm Floyda-Warshalla
 # =================================================================================================
 
 # =================================================================================================
@@ -307,12 +307,12 @@ def DFS_TOPO_VISIT(node, res):
 # =================================================================================================
 # 4. Minimalne drzewo rozpinające (minimum spanning tree):
 # Minimalne drzewo rozpinające to klasyczny problem / algorytm grafowy. Mamy dany graf G(V,E), który ma n wierzchołków, jest 
-# skierowany, a jego krawędzie mają wagi. Chcemy znaleźć minimalne drzewo rozpinające (MST), które łączy wszystkie n wierzchołków
+# nieskierowany, a jego krawędzie mają wagi. Chcemy znaleźć minimalne drzewo rozpinające (MST), które łączy wszystkie n wierzchołków
 # używając minimalnej liczby krawędzi (n-1) o minimalnej sumarycznej wadze. Tych drzew może być kilka, jeśli jest kilka wariantów
 # wyboru tych krawędzi, nas jednak interesuje jakieś jedno rozwiązanie. Aby stworzyć takie rozwiązanie, wpierw spójrzmy na pewien
 # "generyczny" algorytm, który później uzbroimy w konkretne kryteria:
 #
-# - Niech A zbiorem, który będzie trzymał krawędzie minimalnego drzewa rozpinającego. Na początku A jest zbiorem pustym
+# - Niech A będzie zbiorem, który trzyma krawędzie minimalnego drzewa rozpinającego. Na początku A jest zbiorem pustym
 # - Dopóki A nie tworzy MST:
 #   - Znajdź krawędź, która jest "bezpieczna", dodaj ją do A
 # 
@@ -326,4 +326,280 @@ def DFS_TOPO_VISIT(node, res):
 # - Zakończenie: zbiór A zawiera wszystkie krawędzie tworzące pewne MST
 #
 # Wróćmy teraz do zagadnienia bezpiecznych krawędzi. Zacznijmy od wprowadzenia pojęcia przekroju grafu. Będzie ono kluczowe
-# w zrozumieniu logiki stojącą za znajdowaniem krawędzi, którą dodamy do zbioru A. Przekrojem grafu 
+# w zrozumieniu logiki stojącą za znajdowaniem krawędzi, którą dodamy do zbioru A. Przekrojem grafu nazywamy podział tego grafu
+# na dwa podgrafy, a precyzyjniej ujmując sprawę, podział jego wierzchołków. W efekcie tego podziału mamy dwa zbiory: S oraz V-S.
+# Przekrój grafu należy sobie wyobrazić jako linię oddzielającą jedną partię wierzchołków od drugiej. Pochylmy się teraz nad
+# sytuacją związaną z krawędziami:
+#
+# - krawędź (u,v) jest taka, że jeden wierzchołek należy do S a drugi do V-S, to krawędź krzyżuje się z przekrojem. Takie krawędzie
+# nazywamy krawędziami lekkimi.
+# - krawędź (u,v) jest taka, że nie krzyżuje się z przekrojem, czyli należy do jednej ze stron przekroju. Takie krawędzie mają prawo
+# należeć do zbioru A (mają prawo, bo nie muszą jeszcze należeć - przypomnijmy, zbiór A to aktualny podzbiór tworzący MST). 
+# Mówimy, że przekrój uwzględnia zbiór A.
+#
+# Teraz możemy zrozumieć następujące twierdzenie: 
+# Jeśli A to podzbiór MST dla grafu G(V,E), (S,V-S) będzie dowolnym przekrojem uwzględniającym A, (u,v) będzie krawędzią lekką
+# krzyżującą się z przekrojem (S,V-S), to (u,v) jest krawędzią bezpieczną.
+#
+# Dowód tego twierdzenia jest na wykładzie, ale trudno go tu streścić bez rysunków. Z resztą, jest on chyba nieco trudniejszy do
+# zrozumienia niż inne, pewnie nie pojawi się na egzaminie. 
+#
+# Wnioski, jakie można wysnuć z tego twierdzenia są takie: jeśli mamy podzbiór A, który jest zawarty w MST (jeszcze go nie
+# tworzy) to jest on acykliczny, więc jest on zbiorem drzew. Las drzew można przedstawić za pomocą zbioru G_A = (V,A), co ma 
+# dużo sensu, bo bierzemy wszystkie wierzchołki z grafu i łączymy tylko te, które aktualnie są w A - powstawje nam las drzew.
+# Jeśli weźmiemy spójną składową, czyli jedno z drzew C = (V_C,E_C), to jeśli stworzymy krawędź (u,v) łączącą drzewo C z 
+# innym drzewem w lesie G_A, to (u,v) jest krawędzią bezpieczną. Jest tak dlatego, że przekrój (C, V-C) uwzględnia A. 
+#
+# Wracając do generycznego algorytmu MST, teraz go napiszmy w pseudokodzie:
+#
+
+# G to graf, w to funkcja wagi
+def generic_mst(G, w):
+    # A to zbiór pusty
+    A = set()
+    while A != 'MST':
+        (u,v) = G.find_safe_edge(w)
+        A.add(u,v)
+
+#
+# W każdej chwili działania tego algorytmu, krawędzie z A są zawsze acykliczne. G_A = (V,A) jest lasem, gdzie każda spójna składowa
+# jest drzewem, choćby gdyby składały się z jednego wierzchołka (to też spójna składowa). Dodając (u,v) dalej A jest acykliczne, bo
+# łączymy dwa drzewa jedną krawędzią, czyniąc z nich jedno spójne drzewo. Konkretnymi algorytmami, które zajmują się wyborem krawędzi
+# (u,v) to algorytm Kruskala i algorytm Prima. Algorytm Kruskala operuje na lesie G_A, zawsze wybiera najlżejsze krawędzie łączące
+# dwa drzewa. Algorytm Prima operuje na pojedynczym drzewie. Do A zawsze dodawane są krawędzie bezpieczne łączące drzewo A z 
+# pozostałymi wierzchołkami spoza drzewa. Teoretycznie powinienem wpierw powiedzieć o części wykładu opisującej strukturę danych dla
+# zbiorów rozłącznych (disjoint set, union find), ponieważ na tym opiera się algorytm Kruskala, lecz nie mam zbyt dużo czasu, a
+# zrozumienie algorytmu Kruskala nie wymaga dokładnej znajomości, tylko raczej intuicji.
+#
+#   a) Algorytm Kruskala dla znajdowania MST:
+#
+
+def mst_kruskal(G, w):
+    # A to zbiór krawędzi tworzących MST
+    A = set()
+    # G_a to las drzew z wierzchołków G.V
+    G_a = set()
+    # Dla każdego wierzchołka stwórz osobne drzewo rozłączne,
+    # teraz G_a składa się z pojedynczych drzew rozłącznych
+    for v in G.V:
+        G_a.make_set(v)
+    G.E.sort('increasing', w)
+
+    # Idziemy po każdej krawędzi w kolejności rosnącej (wpierw
+    # dodajemy najlżejsze krawędzie)
+    for (u,v) in G.E:
+        # Jeśli wierzchołki należą do różnych drzew rozłącznych
+        if G_a.find_set(u) != G_a.find_set(v):
+            # Aktualizuj MST oraz połącz dwa drzewa w jedno drzewo
+            A.add((u,v))
+            G_a.union(u,v)
+    
+    return A
+
+#
+# Czas działania zależy od tego, jak zaimplementujemy strukturę danych dla zbiorów rozłącznych. Sortowanie krawędzi to
+# O(|E|*lg|E|). Złożoność obliczeniowa tego algorytmu jest dosyć trudna do ustalenia, więc po prostu podam wnioski:
+# Złożoność obliczeniowa: O(|E|*lg|E|), a jeśli przyjmiemy, że |E| < |V|^2, to O(|E|*lg|V|)
+#
+#   b) Algorytm Prima dla znajdowania MST:
+# 
+
+# Nie tylko graf i funkcja wagi, ale też korzeń (wybrany dowolnie)
+def mst_prim(G, w, r):
+    # Najpierw ustaw wszystkie wierzchołki jako nieprzetworzone
+    # Każdy wierzchołek ma atrybuty: klucz (waga krawędzi łącząca go z drzewem
+    # oraz rodzica, czyli z kim ma ową krawędź)
+    for v in G.V:
+        v.key = 'INF'
+        v.parent = 'NIL'
+    # Korzeń drzewa, od niego zaczynamy
+    r.key = 0
+    # Q to kolejka priorytetowa typu min, korzeń jest pierwszy
+    Q = []
+    Q.add(G.V)
+
+    # W kolejce Q są węzły, których jeszcze nie ma w MST
+    while Q.empty != 'True':
+        # Wybierz następny wierzchołek do dodania
+        node = Q.extract_min
+        # Zaaktualizuj każdego z jego sąsiadów...
+        for neighbor in node.neighbors:
+            # ...który jeszcze nie został przetworzony i krawędź łącząca aktualny
+            # węzeł z tym sąsiadem jest mniejsza niż jakaś inna krawędź łącząca ten
+            # wierzchołek z drzewem
+            if neighbor in Q and w(node, neighbor) < neighbor.key:
+                # Teraz ten sąsiad ma aktualne informacje na swój temat, ma rodzica, 
+                # czyli węzeł, który łączy go z drzewem w najbardziej optymalny sposób
+                neighbor.parent = node
+                # Zmiana klucza tutaj aktualizuje pozycje tego wierzchołka w kolejce, być
+                # może stanie się kolejnym elementem do dodania
+                neighbor.key = w(node, neighbor)
+    
+#
+# Dowód poprawności przez niezmiennik pętli:
+# Niezmiennik: przed wykonaniem każdej pętli while zachodzą:
+# - A = {(node, node.parent): node należy do V - {r} - Q}, czyli na MST składają się krawędzie stworzone z wierzchołków
+# oraz ich rodziców zdefiniowanych w algorytmie, które nie należą do kolejki, a także nie są korzeniem (on ma rodzica 'NIL')
+# - Dla każdego node należącego do Q, node.key to koszt krawędzi łączącej go z aktualnym poddrzewem MST, a node.parent to
+# węzeł przez który się łączy (jeśli obie wartości istnieją, mogą jeszcze nie istnieć).
+#
+# Złożoność obliczeniowa algorytmu Prima: opiera się ona na kopcu binarnym, który tworzy się w czasie O(|V|), naprawia w czasie
+# O(lg|V|), aktualizuje klucz w czasie O(|V|), pętlę główną mst_prim wykonujemy |V| razy. Z racji na to, że długość wszystkich
+# list sąsiedztwa (które przetworzymy w pętli for) to 2|E|, to wykonanie pętli for to O(|E|). Łączny czas to:
+#           O(|V|) (budowa) + O(|V|lg|V|) (|V| razy extract) + O(|E|lg|V|) (|E| razy zmiana klucza) = O(|E|lg|V|)
+# Jest to asymptotycznie czas taki sam, jak w algorytmie Kruskala.
+# =================================================================================================
+
+# =================================================================================================
+# 5. Najkrótsze ścieżki z jednym źródłem:
+# Z racji na niewiele czasu przed egzaminem, pominę formalizm i teorię związaną z tymi zagadnieniami, a raczej ograniczę ją do
+# minimum. Po pierwsze, problem znajdowania optymalnej ścieżki między dwoma wierzchołkami posiada własność optymalnej podstruktury.
+# Oznacza to, że jeśli mamy ścieżkę <p_i,p_(i+1),...,p_(j-1),p_j> to każda podścieżka w niej musi być optymalna, by cała ścieżka
+# była optymalna. Po drugie, wierzchołki w grafie będą miały dodatkowe atrybuty na potrzebę znajdowania ścieżek. Po pierwsze, będą
+# miały klucz, który przechowuje koszt dotarcia ze źródła do danego węzła, po drugie, będą miały rodzica, czyli wierzchołek z 
+# którego przyszliśmy. Jeśli już znajdziemy optymalną ścieżkę, to możemy ją wyświetlić w sposób rekurencyjny. Zaczynamy od wierzchołka
+# do którego mieliśmy dojść i podejmujemy decyzję: czy jesteśmy już u źródła? Jeśli tak, to wypisz źródło. Jeśli nie, to wpierw 
+# wypisz element z którego przyszliśmy, a później ten aktualny. 
+#
+
+def print_path(G, v, s):
+    if v == s:
+        print(s)
+    elif v.parent != 'NIL':
+        print_path(G, v.parent, s)
+        print(v)
+    else:
+        print(f'Brak ścieżki z {s} do {v}')
+
+#
+# Nie musimy pochylać się nad złożonością tego algorytmu, lecz jest on oczywiście ograniczony przez długość ścieżki, czyli |V|-1.
+# Dlaczego jednak długość ścieżki jest równa |V|-1? To stwierdzenie zakłada, że nie możemy mieć w grafach cyklu, czyli rozważamy
+# skierowane acykliczne grafy (DAG - directed acyclic graphs). Jeśli graf miały cykl, który sumarycznie dawałby ścieżkę o ujemnej
+# wadze, to zawsze moglibyśmy znaleźć "optymalniejszą" ścieżkę powiększając graf o ten cykl, więc nie rozważamy ujemnych cykli.
+# Co natomiast z dodatnimi cyklami? Cóż, jeśli ze ścieżki usuniemy cykl o dodatniej długości, to uzyskamy ścieżkę, która jest bardziej
+# optymalna, więc nasza najkrótsza ścieżka będzie ścieżką prostą - bez cykli.
+#
+# Aby znaleźć najkrótsze ścieżki z jednego źródła do każdego wierzchołka (nie tylko do jednego, takie założenie algorytmów z tego
+# punktu) należy rozważyć dwa przypadki. Albo dopuszczamy krawędzie o ujemnych wagach, albo tego nie robimy. W pierwszym przypadku
+# będziemy używać algorytmu Bellmana-Forda, który jest algorytmem opartym na DP. Jeśli jednak nie mamy w grafie ujemnych krawędzi,
+# to okazuje się, że algorytm Dijkstry, będący algorytmem zachłannym radzi sobie z problemem wystarczająco. Oba algorytmy opierają
+# się na dwóch procedurach pomocniczych: initialize_source oraz relax. Dlaczego tak jest? Wpierw musimy przygotować nasz graf do
+# pracy z algorytmami, aby to zrobić inicjalizujemy źródło przy pomocy pierwszej procedury:
+#
+
+def initialize_single_source(G, s):
+    for v in G.V:
+        v.key = 'INF'
+        v.parent = 'NIL'
+    s.key = 0
+
+#
+# Jest to dosyć oczywista procedura. Na początku, gdy jeszcze nie pracowaliśmy z grafem i wszystkie wierzchołki były nierozpatrzone
+# to mają wartość w kluczu 'INF', co oznacza, że nie istnieje jeszcze krawędź łącząca dany wierzchołek ze źródłem. Rodzic danego węzła
+# jest równy 'NIL', co oznacza, że nie ma poprzednika z którego byśmy przyszli do danego węzła od źródła. Klucz w źródle jest równy
+# zero, ponieważ nie potrzebujemy się poruszać po żadnej krawędzi aby dotrzeć do źródła.
+#
+# Drugą procedurą jest tak zwana procedura relaksacji wierzchołka. Gdy będziemy odwiedzali dany wierzchołek, relaksacja jest formą
+# aktualizowania informacji na jego temat. Dajmy na to, że z wierzchołka u odwiedzamy wierzchołek v. Jeśli nie był on wcześniej
+# przetworzony, to ma atrybuty v.parent = 'NIL' oraz v.key = 'INF'. Zmieniamy je odpowiednio na 'u' oraz u.key + w(u,v), czyli 
+# koszt dotarcia do tego wierzchołka to koszt dotarcia do 'u', a później koszt przejścia po krawędzi (u,v). Jeśli jednak był on już
+# wcześniej przetworzony, to musimy sprawdzić, czy stary sposób dotarcia do tego wierzchołka jest bardziej, czy mniej optymalny od 
+# naszego nowego sposobu, z węzła 'u'.
+#
+
+def relax(u, v, w):
+    if v.key > u.key + w(u,v):
+        v.key = u.key
+        v.parent = u
+
+#
+# Teraz, uzbrojeni w te dwie metody pomocnicze możemy podjąć się wyzwania znalezienia najkrótszej ścieżki. Zaczniemy od bardziej
+# ogólnego algorytmu, który dopuszcza krawędzie z ujemnymi wagami.
+#
+#   a) Algorytm Bellmana-Forda:
+# Algorytm Bellmana-Forda opiera się na programowaniu dynamicznym, choć nie do końca widać to na pierwszy rzut oka. Służy on nie
+# tylko do znajdowania najkrótszej ścieżki, ale daje również odpowiedź na pytanie o istnienie cyklu o ujemnej wartości w danym grafie.
+# W jaki sposób to robi? W pesymistycznym przypadku uda nam się zrelaksować docelowo (po danej relaksacji uzyskać już globalnie optymalne
+# rozwiązanie w danym węźle) tylko i wyłącznie wierzchołki, które oddalone są o jeden wierzchołek od już aktualnie docelowo zrelaksowanych
+# wierzchołków, więc jeśli chcemy dotrzeć do docelowego wierzchołka oddalonego maksymalnie od źródła (o |V|-1 krawędzi) to potrzebujemy
+# maksymalnie |v|-1 iteracji pętli, która zaraz pojawi się w kodzie. Jeśli więc przekroczymy maksymalną iterację i wejdziemy w kolejną,
+# to jest to sygnał, że wciąż za każdym razem relaksujemy na nowo pewne węzły, ergo występuje cykl o ujemnej wadze. Wychwytywanie
+# najkrótszych ścieżek polega na tym, by podczas każdej z (maksymalnie) |V|-1 iteracji pętli zrelaksować wszystkie możliwe krawędzie.
+# W ten sposób uda nam się znaleźć optymalne rozwiązanie dla każdego wierzchołka. Warto ten algorytm wyposażyć jeszcze w mechanizm
+# aby wyjść przedwcześnie, jeśli okaże się, że podczas danej iteracji nic nie zrelaksowaliśmy, co oznacza, że mamy już optymalne 
+# rozwiązanie naszego problemu.
+#
+
+def bellman_ford(G, w, s):
+    initialize_single_source(G, s)
+    # Główna pętla, szukamy optymalnego rozwiązania
+    for i in range(1, G.V.count):
+        for (u,v) in G.E:
+            relax(u, v, w)
+    # Jeśli znajdziemy jakąkolwiek krawędź, którą wciąż można
+    # zrelaksować, zwracamy False (nie ma rozwiązania)
+    for (u,v) in G.E:
+        if v.key > u.key + w(u,v):
+            return False
+    # W przeciwnym wypadku, rozwiązanie istnieje (jest
+    # zakodowane w rodzicach wierzchołków, mamy napisaną 
+    # procedurę służącą do wyświetlania tej ścieżki)
+    return True
+
+#
+# Złożoność obliczeniowa algorytmu (zakładamy reprezentację grafu przy użyciu list sąsiedztwa) to O(V^2 + VE), a jeśli mamy przypadek
+# że E = Omega(V), czyli E jest ograniczone od dołu V, to złożoność redukuje się do O(VE).
+#
+#   b) Najkrótsze ścieżki w DAG:
+# Jeśli mamy acykliczny graf skierowany (uwaga, Bellman-Ford i Dijkstra radzą sobie z cyklami, Bellman-Ford nawet z ujemnymi, tutaj
+# zakładamy, że nie ma cyklu), to możemy na nim zrobić sortowanie topologiczne, co sprowadzi go (wizualnie, oczywiście) do pewnej
+# listy zależności. Możemy w szybki sposób obliczyć najkrótsze ścieżki przy pomocy 'listy' zwracanej przez sortowanie topologiczne:
+#
+
+def DAG_shortest_path(G, s, w):
+    g_list = G.topological_sort
+    # Dla każdego wierzchołka w kolejności topologicznej
+    for u in g_list:
+        # Dokonaj relaksacji na jego sąsiadach
+        for neighbor in u.neighbors:
+            relax(u, neighbor, w)
+
+#
+# Dokonanie relaksacji na sąsiadach wystarczy, gdyż o to chodzi w sortowaniu topologicznym, by ustawić wierzchołki w takiej 
+# kolejności, by każdy wierzchołek x miał przed sobą wierzchołki od których zależy. W ten sposób pominiemy wszystkie wierzchołki
+# nieosiągalne ze źródła, później relaksujemy wierzchołki w naturalnej kolejności, biorąc pod uwagę ich poprzedników.
+#
+# Złożoność obliczeniowa: Theta(V+E)
+#
+#   c) Algorytm Dijkstry:
+# Algorytm Dijkstry jest uogólnieniem BFS. W przeszukiwaniu wszerz używaliśmy kolejki FIFO opartej na jednostkowej odległości węzła
+# od źródła, co tutaj nie wystarczy. Musimy zamienić atrybut 'distance' na 'key', tak jak to robiliśmy w Bellmanie-Fordzie, oraz
+# wprowadzić kolejkę priorytetową. Dijkstry używamy, gdy mamy do czynienia z grafem o samych dodatnich krawędziach co sprawia, że
+# możemy zastosować strategię zachłanną. Za każdym razem wyciągamy z kolejki priorytetowej element o najmniejszyym kluczu i 
+# relaksujemy jego sąsiadów, co przypomina też algorytm Prima.
+#
+
+def dijkstra(G, s, w):
+    initialize_single_source(G, s)
+    # S to już odwiedzone wierzchołki
+    S = set()
+    # Q to kolejka priorytetowa typu min, dodaj
+    # wszystkie wierzchołki
+    Q = create_queue(G.V)
+    while Q.empty != True:
+        u = Q.dequeue()
+        S.add(u)
+        # Relaksacja każdego sąsiada, aktualizacja
+        # kolejki priorytetowej
+        for v in u.neighbors:
+            relax(u, v, w)
+
+#
+# Złożoność obliczeniowa algorytmu Dijkstry zależy od implementacji kolejki priorytetowej. Dla zwykłej tablicy będzie ona wynosiła
+# O(V^2), dla kopca binarnego (klasyka) będzie wynosiła O((V+E)lgV). Jest jeszcze jakiś kopiec Fibonacciego, lecz nawet nie będę
+# o nim pisać, bo nie był on zbytnio omawiany.
+# =================================================================================================
+
+# =================================================================================================
+# 6. Najkrótsze ścieżki między wszystkimi parami wierzchołków:
+# 
